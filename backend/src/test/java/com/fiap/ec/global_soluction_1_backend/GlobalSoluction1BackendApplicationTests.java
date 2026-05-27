0f391b92764aa9464f8c708701844a0702927f83
Mario S <mario2930874@gmail.com>
2026-05-27 19:28:43 -0300

    feat: initial commit com base do Spring Boot

---
+++ backend/src/test/java/com/fiap/ec/global_soluction_1_backend/GlobalSoluction1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.ec.global_soluction_1_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalSoluction1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
