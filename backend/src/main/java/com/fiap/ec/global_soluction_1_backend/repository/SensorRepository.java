bea77c12773591b79bb60396cfaa958fba73c98e
Luis Fernando Picarelli <lfpicarelli@gmail.com>
2026-05-27 20:39:48 -0300

    feat: adiciona interface SensorRepository

---
+++ backend/src/main/java/com/fiap/ec/global_soluction_1_backend/repository/SensorRepository.java
@@ -0,0 +1,6 @@
+package com.fiap.ec.globalsoluction1backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.fiap.ec.globalsoluction1backend.model.Sensor;
+
+public interface SensorRepository extends JpaRepository<Sensor, Long>
