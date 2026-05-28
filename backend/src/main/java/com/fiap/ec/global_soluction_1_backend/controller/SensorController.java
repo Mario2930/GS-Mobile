5e33af312291ee994115c651f0c80b3d85afa384
Mario S <mario2930874@gmail.com>
2026-05-28 16:55:35 -0300

    feat: cria rotas da API em SensorController

---
+++ backend/src/main/java/com/fiap/ec/global_soluction_1_backend/controller/SensorController.java
@@ -0,0 +1,53 @@
+package com.fiap.ec.globalsoluction1backend.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fiap.ec.globalsoluction1backend.model.Sensor;
+import com.fiap.ec.globalsoluction1backend.service.SensorService;
+
+@RestController
+@RequestMapping("/sensores")
+@CrossOrigin(origins = "*")
+public class SensorController {
+    
+    private final SensorService service;
+
+    public SensorController(SensorService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Sensor criar(@RequestBody Sensor sensor) {
+        return service.salvar(sensor);
+    }
+
+    @GetMapping
+    public List<Sensor> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Sensor buscarPorId(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public Sensor atualizar(@PathVariable Long id, @RequestBody Sensor sensor) {
+        return service.atualizar(id, sensor);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
+}
